f12b75eaaf36c54e415671a7d410eff445b9ce79
onur <ionuraygan@gmail.com>
2022-12-20 02:32:54 +0300

    Update

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,126 +1,21 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import axios from "axios";
-import "./Main.css";
-import profilePicture from "../../assets/images/profilePicture.png";
-import CloseIcon from "../../assets/images/icon-menu-close.svg";
-import MenuIcon from "../../assets/images/icon-menu.svg";
 import logout from "../../assets/images/logout-svgrepo-com.svg";
-import SearchIcon from "@mui/icons-material/Search";
-import SettingsIcon from "@mui/icons-material/Settings";
-import { footerItem, table } from "./constant";
+import "./Main.css";
+import CloseIcon from "../../assets/images/icon-menu-close.svg";
+import { footerItem} from "./constant";
 import { sidebarItem } from "../LeftSidebar/constants";
-import {openHamburgerMenu, closeHamburgerMenu} from '../../utils/utils'
+import { closeHamburgerMenu} from '../../utils/utils'
+import Table from "../Table/Table";
+import Header from "../Header/Header";
 
 
 function Main() {
-  const [data, setData] = useState([]);
-
-  async function getData() {
-    try {
-      const response = await axios.get("https://dummyjson.com/users");
-      const data = response.data.users.slice(0, 3);
-      setData(data);
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
-  useEffect(() => {
-    getData();
-  }, []);
 
   return (
     <div className="main-wrapper">
-      <header className="header">
-        <div className="search-bar">
-          <SearchIcon id="search-icon" />
-          <input
-            className="search-input"
-            type="text"
-            placeholder="Enter your search request"
-          />
-          <SettingsIcon />
-        </div>
-        <div className="consultant">
-          Get consultant
-          <div className="consultant-image">
-            <img
-              src={profilePicture}
-              width="40px"
-              height="40px"
-              alt="Get consultant"
-            />
-          </div>
-        </div>
-        <img
-          src={MenuIcon}
-          onClick={openHamburgerMenu}
-          className="hamburger-menu-open"
-          width="20px"
-          alt=""
-        />
-      </header>
-      <div className="main-section">
-        <div className="left-wrapper">
-          <section id="order-history">
-            <h2 class="section-title">Order History</h2>
-            <p class="section-description">
-              Manage billing information and view receips
-            </p>
-            <div class="section-content">
-              <div class="table">
-                <div class="row">
-                  <div class="column">
-                    <h2>Date</h2>
-                  </div>
-                  <div class="column">
-                    <h2>Type</h2>
-                  </div>
-                  <div class="column">
-                    <h2>Receipt</h2>
-                  </div>
-                </div>
-                {table.map((item) => (
-                  <div class="row" key={item.id}>
-                    <div class="column">
-                      <p>{item.date}</p>
-                    </div>
-                    <div class="column">
-                      <p>{item.type}</p>
-                    </div>
-                    <div class="column">
-                      <button>Download</button>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </div>
-          </section>
-        </div>
-        <div className="right-wrapper">
-          <div className="right-up-wrapper">
-            <img src={profilePicture} width="96px" height="96px" alt="" />
-            <h1>Sync with your </h1>
-            <h1 id="up-wrapper-header">medical record</h1>
-            <p>Keep your doctor </p>
-            <p>informed abort the</p>
-            <p>condition</p>
-            <button>Sync</button>
-          </div>
-          <div className="right-down-wrapper">
-            <h1>Consultations</h1>
-            {data?.map((item) => (
-              <div className="consultant-item" key={item.id}>
-                <img src={item.image} width="25px" alt="" />
-                <div className="description">
-                  <h2>Dr. {item.firstName}</h2>
-                  <p>{item.bank.cardExpire}</p>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
+      <Header />
+      <Table />
       <div className="footer">
         <div className="footer-header">
           <h1>Prescribed</h1>
